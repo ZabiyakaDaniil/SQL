d73a1ede1b92b7be922a1b3fdb57c3bed98fbede
ZabiyakaDaniil <101666279+ZabiyakaDaniil@users.noreply.github.com>
2023-04-07 11:04:56 +0300

    Update trip.sql

--- Stepik.org/SQL simulator/Trip/trip.sql
+++ Stepik.org/SQL simulator/Trip/trip.sql
@@ -6,7 +6,6 @@ select name, city, per_diem, date_first, date_last
 from trip
 where name like '%а _._.'
 order by date_last DESC;
-order by price DESC;
 
 --Вывести в алфавитном порядке фамилии и инициалы тех сотрудников, которые были в командировке в Москве.
 
